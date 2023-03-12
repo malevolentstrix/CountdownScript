0b606e65e3014c49326882fe68665a8c7b417cf9
Jithin John <jithintvm1@gmail.com>
2023-03-12 11:36:30 +0530

    Update run.py

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ from discord import SyncWebhook
 def scheduled_job():
 
     today = datetime.date.today()
-    future = datetime.date(2023, 6, 9)
+    future = datetime.date(2023, 6, 10)
     diff = future - today
     day = diff.days
     temp = day
