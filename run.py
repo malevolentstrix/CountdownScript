3ba92ec7eea90585ba2ebda90c25708ccb5a2ea7
Jithin John <jithintvm1@gmail.com>
2023-03-15 21:26:16 +0530

    Update run.py

--- run.py
+++ run.py
@@ -9,16 +9,25 @@ def scheduled_job():
 
     today = datetime.date.today()
     future = datetime.date(2023, 6, 20)
+    future2 = datetime.date(2023, 5, 15)
     diff = future - today
+    diff2 = future2 - today
+    day2 = diff.days
     day = diff.days
     temp = day
+    temp2 = day2
     if(day > 30):
         month = int(day/30)
         day = int(day % 30)
+    if(day2 > 30):
+        month2 = int(day2/30)
+        day2 = int(day2 % 30)
 
     # FOR TG
     base_url2 = 'https://api.telegram.org/bot5481709060:AAHiCCyL9ZISkf7iXl3w10hyK2Lt049XLfQ/sendMessage?chat_id=-945979691&text={}'.format(
-        str(month) + " months " + str(day) + " days (" + str(temp) + " days) for " + str(future))
+        str(month) + " months " + str(day) + " days (" + str(temp) + " days) for Placement Day " + str(future))
+    base_url2 = 'https://api.telegram.org/bot5481709060:AAHiCCyL9ZISkf7iXl3w10hyK2Lt049XLfQ/sendMessage?chat_id=-945979691&text={}'.format(
+        str(month2) + " months " + str(day2) + " days (" + str(temp2) + " days) for SA Certification Day " + str(future2))
 
     base_url = 'https://api.telegram.org/bot5481709060:AAHiCCyL9ZISkf7iXl3w10hyK2Lt049XLfQ/sendPoll'
 
