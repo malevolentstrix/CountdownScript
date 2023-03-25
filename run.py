eb19dcd2462d1cafa8435ab1c940ef44f0455f74
malevolentstrix <jithintvm1@gmail.com>
2023-03-26 03:12:04 +0530

    add gspread support and tg reply support

--- run.py
+++ run.py
@@ -1,57 +1,93 @@
 import requests
 import datetime
+from datetime import timedelta
 import requests
-import json
-# from discord import SyncWebhook
+from google.oauth2.service_account import Credentials
+from googleapiclient.errors import HttpError
+from googleapiclient.discovery import build
+today = datetime.date.today()
+
+def writetosheet():
+
+    # Replace with the ID of your Google Sheet
+    SPREADSHEET_ID = '1zpqe3bmrhTnmbgjPxHxZbNyGBaPx8OjlCpYIMW8w-qE'
+    # Replace with the name of your sheet
+    SHEET_NAME = 'Daily Status'
+
+    # Replace with the path to your service account JSON file
+    creds = Credentials.from_service_account_file(
+        './t2gd-352012-9f4db7e21863.json',
+        scopes=['https://www.googleapis.com/auth/spreadsheets']
+    )
+
+    # Authenticate with the Google Sheets API
+    service = build('sheets', 'v4', credentials=creds)
+
+    # Get the values of the first sheet
+    try:
+        result = service.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID, range=SHEET_NAME).execute()
+        values = result.get('values', [])
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        values = None
+
+    # Get the range for the first empty row
+    if values:
+        next_row = len(values) + 1
+    else:
+        next_row = 1
+    range_name = f"{SHEET_NAME}!A{next_row}"
+
+    # Write the values to the sheet
+    if range_name:
+        body = {
+            'values': [[str(today-timedelta(days= 1)), "gduyss"]],
+            'majorDimension': 'ROWS'
+        }
+        try:
+            result = service.spreadsheets().values().update(spreadsheetId=SPREADSHEET_ID, range=range_name, valueInputOption='USER_ENTERED', body=body).execute()
+            print(f"{result.get('updatedCells')} cells updated.")
+        except HttpError as error:
+            print(f"An error occurred: {error}")
+
+            
+
 
 
 def scheduled_job():
 
-    today = datetime.date.today()
+
+
+    # Check yesterday's problem solution
+    url = "https://api.telegram.org/bot5481709060:AAHiCCyL9ZISkf7iXl3w10hyK2Lt049XLfQ/getUpdates"
+
+    # Make a request to the API
+    response = requests.get(url)
+    print(response.content)
+    # Extract the message text from the response
+    for i in response.json()["result"]:
+        if i["message"]["reply_to_message"]["message_id"] == chatid_prevday:
+            message_text = i["message"]["text"]
+
+
+    
     future = datetime.date(2023, 6, 20)
-    future2 = datetime.date(2023, 5, 15)
     diff = future - today
-    diff2 = future2 - today
-    day2 = diff2.days
     day = diff.days
     temp = day
-    temp2 = day2
-    if(day > 30):
+    if (day > 30):
         month = int(day/30)
         day = int(day % 30)
-    if(day2 > 30):
-        month2 = int(day2/30)
-        day2 = int(day2 % 30)
 
-    # FOR TG
+
     base_url2 = 'https://api.telegram.org/bot5481709060:AAHiCCyL9ZISkf7iXl3w10hyK2Lt049XLfQ/sendMessage?chat_id=-945979691&text={}'.format(
         str(month) + " months " + str(day) + " days (" + str(temp) + " days) for Placement Day " + str(future))
-    base_url3 = 'https://api.telegram.org/bot5481709060:AAHiCCyL9ZISkf7iXl3w10hyK2Lt049XLfQ/sendMessage?chat_id=-945979691&text={}'.format(
-        str(month2) + " months " + str(day2) + " days (" + str(temp2) + " days) for SA Certification Day " + str(future2))
+    
+    # chatid = requests.get(base_url2)
+    # print(chatid.content)
 
-    base_url = 'https://api.telegram.org/bot5481709060:AAHiCCyL9ZISkf7iXl3w10hyK2Lt049XLfQ/sendPoll'
+    writetosheet()
 
-    parameters = {
-        "chat_id" : "-945979691",
-        "question" : "To do",
-        # Regular Day
-        # "options" : json.dumps(["Solve 10 LeetCode Problems", "Solve 10 Aptitude Problems", "Learn AWS", "Read 10 Pages", "Speak 15 mins", "Solve Rubiks Cube"]),
-        
-        # Rush Days
-        "options" : json.dumps(["Final Year Project", "Wordle", "Photo Album", "Tic Tac Toe", "Exam Prep"]),
-        "allows_multiple_answers" : True
-    }
-
-    resp = requests.get(base_url, data = parameters)
-    print(resp)
-
-    requests.get(base_url2)
-    requests.get(base_url3)
-
-    # FOR DISCORD
-    # webhook = SyncWebhook.from_url(
-    #     "https://discord.com/api/webhooks/1013058619358597270/tbBUf22vy2fIbKo_K_bgIPuD6L57Z7ueWBrbvzXYS96bfWQyKmuE9XV0E3q-5Gjuf6GF")
-    # webhook.send(str(month) + " months " + str(day) + " days (" + str(temp) + " days)")
 
 
 scheduled_job()
